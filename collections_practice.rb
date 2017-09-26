2d9a33ceba78720bd2157287050b8eaadea563b5
John Tuttle <thejwt@gmail.com>
2017-09-26 05:05:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,3 +36,12 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  array.each_with_index do |element, index|
+    placeholder_value = element.split
+    placeholder_value[2] = "$"
+    placeholder_value.join
+    array[index] = placeholder_value
+  end
+end
